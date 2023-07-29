b527d11fd6c05d20d55e4c1d181f716dcb83fae3
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-07-29 18:32:06 +0430

    (Add):<-Loading->

---
+++ app/loading.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { CircularProgress, Stack } from '@mui/material';
+const loading = () => {
+  return (
+    <Stack
+      sx={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <CircularProgress color="secondary" size="3rem" />
+    </Stack>
+  );
+};
+
+export default loading;
